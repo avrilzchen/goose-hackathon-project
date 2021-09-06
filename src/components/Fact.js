c176a209152af064c3619589dcf87838aa065d82
Brandon Gartner <brandonofthegartner@gmail.com>
2021-09-06 19:52:38 -0400

    did all basic setup

---
+++ src/components/Fact.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function getFunFact(SELECTOR) {
+    switch(SELECTOR){
+        case "SPECIES":
+            return 'There are about 30 species of geese worldwide.';
+        case "EYES":
+            return 'Geese can sleep with one eye open, only needing half their brain to shut down to rest.';
+        case "DISTANCE":
+            return 'They fly up to 5000km when migrating.';
+        case "SPEED":
+            return 'Geese fly at speed of about 65km/hr, but can go up to 110km/h if they catch a strong tailwind.';
+        case "LIFESPAN":
+            return 'Geese live anywhere from 10 to 25 years.';
+        case "LOYALTY":
+            return 'Geese are loyal creatures and mate for life. They’re very protective of their partners and offspring, refusing to leave their side if something goes wrong.';
+        case "AGE":
+            return 'Geese existed some 10-12 million years ago.';
+        default:
+            return 'bruh something is wrong';
+    }
+}
+
+function Fact(props){
+
+    var factText = getFunFact(props.factText);
+    console.log(factText);
+    return(
+        <p className={"fact"}>
+            {factText}
+        </p>
+    )
+}
+
+export default Fact;
+
---
+++ src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import {Header} from '../components/Header';
+import Fact from '../components/Fact';
+
+            // <Header />
+function MainPage(){
+    return (
+        <div>
+            <Fact 
+                factText = "EYES" 
+            />
+        </div>
+    )
+}
+
+export default MainPage;
